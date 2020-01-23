5672f7bdbdca037ce9397f6202fc20d548d54767
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:26:53 -0800

    Added default CDrivePathProvider service implementation.

---
+++ source/R5T.Copenhagen.Default/Code/Services/Implementations/CDrivePathProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using R5T.Canterbury;
+
+
+namespace R5T.Copenhagen.Default
+{
+    public class CDrivePathProvider : ICDrivePathProvider
+    {
+        private ICDriveNameConvention CDriveNameConvention { get; }
+
+
+        public CDrivePathProvider(ICDriveNameConvention cDriveNameConvention)
+        {
+            this.CDriveNameConvention = cDriveNameConvention;
+        }
+
+        public string GetCDrivePath()
+        {
+            var cDriveName = this.CDriveNameConvention.GetCDriveName();
+            return cDriveName;
+        }
+    }
+}
